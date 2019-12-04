b35b30ee4aec1fcfd92475ff2c4c70334966107e
jmanly56 <no@example.com>
2019-12-04 11:22:03 -0800

    Copied over test functions

--- src/testclass.cpp
+++ src/testclass.cpp
@@ -13,33 +13,46 @@ TestClass::~TestClass()
 
 }
 
-void TestClass::testCollision(Game &g)
+void TestClass::testCollision(Game& g)
 {
-    Enemy *e = g.createEnemy(1, 1000, 100, 1);
-    e->setPosition(WIDTH/2, HEIGHT/2);
-    g.graphics->createTexture("enemy.png",  *e);
-    e->setOrigin(e->getLocalBounds().width/2 ,e->getLocalBounds().height/2);
-    g.gameObjects.push_back(e);
+	Enemy* e = g.createEnemy(1, 1000, 100, 1);
+	e->setPosition(15, HEIGHT / 2);
+	g.graphics->createTexture("enemy.png", *e);
+	e->setOrigin(e->getLocalBounds().width / 2, e->getLocalBounds().height / 2);
+	g.gameObjects.push_back(e);
 }
 
-void TestClass::testScore(Game &g)
+void TestClass::testScore(Game& g)
 {
-    g.scoreNum += 10000;
+	g.scoreNum += 10000;
 }
 
-void TestClass::testMovement(Game &g)
+void TestClass::testMovement(Game& g)
 {
-    if(clock.getElapsedTime().asSeconds() == 2.f && hasMoved == false)
-    {
-        g.player->setPosition(20, g.player->getPosition().y);
-        hasMoved = true;
-    }
+
+	if (g.player->getPosition().x >= 15)
+	{
+		g.player->updatePosition(-(g.player->getMoveSpeed() / 4), 0, g.getDeltaTime());
+
+		if (g.player->getPosition().x < 15)
+		{
+			g.player->setPosition(15, g.player->getPosition().y);
+		}
+	}
 }
 
-
-void TestClass::testProjectile(Game &g)
+void TestClass::testProjectile(Game& g)
 {
-
+	if (g.player->getPosition().x >= 31)
+	{
+		//shoot
+		Projectile* p = g.player->shoot();
+		if (p != NULL)
+		{
+			g.projectiles.push_back(p);
+			g.audio->playShoot(*g.player);
+		}
+	}
 }
 
 void TestClass::testDamage(Game &g)
